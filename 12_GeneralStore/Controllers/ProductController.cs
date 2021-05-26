d6eb2de9d3981ebc28f6dc216b02f871c7beb8c0
Alexandro Cazares <CazaresCode@gmail.com>
2021-05-26 15:52:15 -0400

    added controllers and entities.

---
+++ 12_GeneralStore/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using _12_GeneralStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace _12_GeneralStore.Controllers
+{
+    public class ProductController : ApiController
+    {
+
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById([FromUri] int id)
+        {
+            Product product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateProduct(int id, Product updatedProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                Product product = await _context.Products.FindAsync(id);
+
+                if (product!= null)
+                {
+                    product.Name = updatedProduct.Name;
+                    product.Price = updatedProduct.Price;
+                    product.UPC = updatedProduct.UPC;
+                    product.Quantity = updatedProduct.Quantity;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+
+                return NotFound();
+            }
+
+            return BadRequest();
+        }
+    }
+}
